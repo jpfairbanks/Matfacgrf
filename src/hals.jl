fc260191cea17afea0c96e003942bd4fda03d2ee
James Fairbanks <james.fairbanks@gatech.edu>
2014-03-14 20:45:02 -0400

    add reference for hals.jl and new parameter type matchin multupd.jl

--- src/hals.jl
+++ src/hals.jl
@@ -1,4 +1,42 @@
+#
+# Hierarchical Alternating Least Squares Algorithm for NMF.
+#
+# Reference:
+# @article{cichocki2009fast,
+#   title={Fast local algorithms for large scale nonnegative matrix and tensor factorizations},
+#   author={Cichocki, Andrzej and Anh-Huy, PHAN},
+#   journal={IEICE transactions on fundamentals of electronics, communications and computer sciences},
+#   volume={92},
+#   number={3},
+#   pages={708--721},
+#   year={2009},
+#   publisher={The Institute of Electronics, Information and Communication Engineers},
+#   url={http://www.bsp.brain.riken.jp/publications/2009/Cichocki-Phan-IEICE_col.pdf}
+# }
+#
+# original MATLAB implementation by Ramakrishnan Kannan at Georgia Tech.
+# original Julia implementation by James Fairbanks at Georgia Tech.
+
 using NMF
+type HierarchicalALS
+    maxiter::Int    # maximum number of iterations
+    verbose::Bool   # whether to show procedural information
+    tol::Float64    # threshold for convergence
+    lambda::Float64 # regularization parameter default 0
+
+    function HierarchicalALS(;maxiter::Integer=100,
+                             verbose::Bool=false,
+                             tol::Real=1.0e-6,
+                             lambda::Real=0.0)
+        
+        # check some constraints on the parameters.
+        maxiter >= 1 || error("maxiter must be at least 1.")
+        tol > 0 || error("tolerance must be positive.")
+        # TODO: are there any constraints on lambda?
+        #lambda >= || error("lambda must be nonnegative.")
+    end
+end
+
 function hals(A, Winit, Hinit, k, tolerance, maxiter, beta=0)
     #hals stands for Hierarchical Alternating Least Squares
     #solves A=WH
