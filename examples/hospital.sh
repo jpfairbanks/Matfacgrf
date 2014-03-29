a381d675deaab162ffd0c5b523916d3ddc980a31
James <jpfairbanks@gmail.com>
2014-03-29 10:28:20 -0400

    Run correlation demo as part of hospital analysis

--- examples/hospital.sh
+++ examples/hospital.sh
@@ -1,2 +1,4 @@
 julia=~/julia/julia
 $julia examples/visualize.jl -f data/hospital_edges.csv -k 6 --batchsize 3000 --max_vertices 75
+printf "Running correlation study\n"
+$julia -e 'reload("src/correlation.jl"); test();'
